a9e2d2b37755311246ef2b5ffe64cce673a40f42
Diane Tuyizere <dtuyizere@smith.edu>
2025-11-02 14:23:30 -0500

    Added in the constructor for the house and the other attributes.

--- House.java
+++ House.java
@@ -1,7 +1,40 @@
-/* This is a stub for the House class */
-public class House {
 
-  public House() {
+import java.util.jar.Attributes;
+import java.util.ArrayList;
+//public class House implements HouseRequirements{
+public class House extends Building{
+  protected ArrayList<Student> residents;
+  protected boolean hasDiningRoom;
+  // constructor
+public House(String Name, String Address, int nFloors,ArrayList<Student> residents, boolean hasDiningRoom){
+  super(Name, Address, nFloors);
+  this.residents=residents;
+  this.hasDiningRoom=hasDiningRoom;
+}
+public boolean gethasDiningRoom()
+{
+  return this.hasDiningRoom();
+}
+  
+];
+public int nResidents();
+public void moveIn(Student s);
+public Student moveOut(Student s);
+
+
+}
+
+
+
+
+/* This is a stub for the House class */
+
+  House extend building;
+  
+
+  public House(ArrayList<Student> residents,boolean hasDiningRoom) {
+    super( Name, Address, nFloors);
+
     System.out.println("You have built a house: 🏠");
   }
 
@@ -9,4 +42,5 @@ public class House {
     new House();
   }
 
+}
 }
