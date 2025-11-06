8ffd2aa43ce636edfa4b209891636615ad2004d8
Diane Tuyizere <dtuyizere@smith.edu>
2025-11-06 18:32:42 -0500

    Made the house( now it's running and some part of library)

--- House.java
+++ House.java
@@ -1,46 +1,54 @@
 
 import java.util.jar.Attributes;
 import java.util.ArrayList;
-//public class House implements HouseRequirements{
-public class House extends Building{
+//public House extends Building and implements HouseRequirements
+public class House extends Building implements HouseRequirements{
   protected ArrayList<Student> residents;
   protected boolean hasDiningRoom;
   // constructor
-public House(String Name, String Address, int nFloors,ArrayList<Student> residents, boolean hasDiningRoom){
-  super(Name, Address, nFloors);
-  this.residents=residents;
+public House(String name, String Address, int nFloors, boolean hasDiningRoom){
+  super(name, Address, nFloors);
+  this.residents=new ArrayList<Student>();
   this.hasDiningRoom=hasDiningRoom;
+  System.out.println("You have built a house: 🏠");
 }
-public boolean gethasDiningRoom()
+public boolean hasDiningRoom()
 {
-  return this.hasDiningRoom();
+  return this.hasDiningRoom;
 }
-  
-];
-public int nResidents();
-public void moveIn(Student s);
-public Student moveOut(Student s);
-
-
+public int nResidents(){
+return this.residents.size();
 }
+// Adding and remo
+public void moveIn(Student s){
+  this.residents.add(s);
+};
+public boolean isResident(Student s) {
+  if (residents.isEmpty()) {
+    throw new RuntimeException("House is empty");
+  }
 
+  if (this.residents.contains(s)) {
+    return true;
+  } else {
+    throw new RuntimeException(s + " is not in this house");
+  }
+}
+public Student moveOut(Student s){
+  if (isResident(s)){
+    this.residents.remove(s);
+    return s;
+  }
+   else {
+    throw new RuntimeException(s + " is not in this house");
+  }
+}
 
 
 
 /* This is a stub for the House class */
-
-  House extend building;
-  
-
-  public House(ArrayList<Student> residents,boolean hasDiningRoom) {
-    super( Name, Address, nFloors);
-
-    System.out.println("You have built a house: 🏠");
-  }
-
   public static void main(String[] args) {
-    new House();
+    House myHouse= new House("Cutter","10 Prospect Street",3,true);
   }
 
-}
 }
--- Library.java
+++ Library.java
@@ -1,9 +1,32 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
+import java.util.Hashtable;
+
+public class Library extends Building implements LibraryRequirements{
+  private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String Address, int nFloors) {
+      super(name, Address, nFloors);
+      this.collection= new Hashtable<String,Boolean>();
       System.out.println("You have built a library: 📖");
     }
+  public void addTitle(String title){
+    this.collection.put(title, true);
+
+  }
+  public String removeTitle(String title){
+    this.collection.remove(title);
+    return title;
+
+  }
+  public void checkOut(String title){
+    this.collection.put(title, false);
+
+  }
+  public void returnBook(String title){
+    this.collection.put(title, true);
+
+  }
   
     public static void main(String[] args) {
       new Library();
